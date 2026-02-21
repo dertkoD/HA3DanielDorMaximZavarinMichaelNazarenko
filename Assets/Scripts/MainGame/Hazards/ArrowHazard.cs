f567affac0a32eda161cb760b779b63cd0c665c4
Mike-Avrius <mike.nidam@gmail.com>
2026-02-21 17:01:50 +0200

    Get comonent delete

--- Assets/Scripts/MainGame/Hazards/ArrowHazard.cs
+++ Assets/Scripts/MainGame/Hazards/ArrowHazard.cs
@@ -20,7 +20,7 @@ public class ArrowHazard : MonoBehaviour
         shootIntervalLeft -= Time.deltaTime;
         if (shootIntervalLeft <= 0)
         {
-            ArrowObject arrow = Instantiate(arrowPrefab,transform.position,Quaternion.identity).GetComponent<ArrowObject>();
+            GameObject arrow = Instantiate(arrowPrefab,transform.position,Quaternion.identity);
             arrow.transform.Rotate(0,90,0);
             arrow.transform.Rotate(0,90,0);
             shootIntervalLeft = shootInterval;
