c4dc111bce43ce69e27a3850550d80ce709b9984
Mike-Avrius <mike.nidam@gmail.com>
2026-02-23 10:40:07 +0200

    Arrow Pool

--- Assets/Scripts/MainGame/Hazards/ArrowHazard.cs
+++ Assets/Scripts/MainGame/Hazards/ArrowHazard.cs
@@ -3,26 +3,29 @@ using UnityEngine;
 
 public class ArrowHazard : MonoBehaviour
 {
-    public GameObject arrowPrefab;
+    [SerializeField] private ArrowPool arrowPool;
+
     [SerializeField] float shootInterval;
     private float shootIntervalLeft;
-    
-  
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         shootIntervalLeft = shootInterval;
     }
 
-    // Update is called once per frame
     void Update()
     {
         shootIntervalLeft -= Time.deltaTime;
+
         if (shootIntervalLeft <= 0)
         {
-            GameObject arrow = Instantiate(arrowPrefab,transform.position,Quaternion.identity);
-            arrow.transform.Rotate(0,90,0);
-            arrow.transform.Rotate(0,90,0);
+            GameObject arrow = arrowPool.GetArrow();
+
+            arrow.transform.position = transform.position;
+            arrow.transform.rotation = Quaternion.identity;
+
+            arrow.transform.Rotate(0, 180, 0);
+
             shootIntervalLeft = shootInterval;
         }
     }
